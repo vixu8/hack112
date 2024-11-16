3d4d25dbbca128e27e4a85bd9185b3df5d1407a2
cjin3 <christopherqjin@gmail.com>
2024-11-15 21:54:46 -0500

    creating the map

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from types import SimpleNamespace
 import os
 import time
 import random
+from map import *
 
 msg = "Roll for initiative:"
 print(msg)
@@ -14,37 +15,41 @@ def onAppStart(app):
     restart(app)
 
 def restart(app):
-    app.row, app.col = 5
+    app.rows = app.cols = 5
+    changeMapWidth(app)
+    changeMapHeight(app)
+    app.map = Map(app.rows, app.cols)
+    print(app.map)
 
 def changeMapHeight(app):
     height = app.getTextInput('Enter the height of the map: ')
-    isInt = isinstance(height, int)
+    isInt = height.isdigit()
     if not isInt:
         app.showMessage('Please enter an integer!')
         changeMapHeight(app)
         return
     height = int(height)
     moreThan5 = height >= 5
-    elif not moreThan5:
+    if not moreThan5:
         app.showMessage('Please enter a number greater than 5!')
         changeMapHeight(app)
         return
-    app.row = height
+    app.rows = height
 
 def changeMapWidth(app):
     width = app.getTextInput('Enter the width of the map: ')
-    isInt = isinstance(width, int)
+    isInt = width.isdigit()
     if not isInt:
         app.showMessage('Please enter an integer!')
         changeMapWidth(app)
         return
     width = int(width)
     moreThan5 = width >= 5
-    elif not moreThan5:
+    if not moreThan5:
         app.showMessage('Please enter a number greater than 5!')
         changeMapWidth(app)
         return
-    app.row = width
+    app.cols = width
 
 
 def redrawAll(app):
