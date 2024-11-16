1d213681091b71f65d5e2dc1a80720353ab7435c
cjin3 <christopherqjin@gmail.com>
2024-11-15 21:41:40 -0500

    height width setters

--- main.py
+++ main.py
@@ -14,10 +14,38 @@ def onAppStart(app):
     restart(app)
 
 def restart(app):
-    app.row, app.col = 0
+    app.row, app.col = 5
 
-def promptUser(app):
-    
+def changeMapHeight(app):
+    height = app.getTextInput('Enter the height of the map: ')
+    isInt = isinstance(height, int)
+    if not isInt:
+        app.showMessage('Please enter an integer!')
+        changeMapHeight(app)
+        return
+    height = int(height)
+    moreThan5 = height >= 5
+    elif not moreThan5:
+        app.showMessage('Please enter a number greater than 5!')
+        changeMapHeight(app)
+        return
+    app.row = height
+
+def changeMapWidth(app):
+    width = app.getTextInput('Enter the width of the map: ')
+    isInt = isinstance(width, int)
+    if not isInt:
+        app.showMessage('Please enter an integer!')
+        changeMapWidth(app)
+        return
+    width = int(width)
+    moreThan5 = width >= 5
+    elif not moreThan5:
+        app.showMessage('Please enter a number greater than 5!')
+        changeMapWidth(app)
+        return
+    app.row = width
+
 
 def redrawAll(app):
     pass
