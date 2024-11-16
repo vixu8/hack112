d1f10988d12975b10ae9b4818fee9f542d502802
vixu8 <viola.r.xu@gmail.com>
2024-11-15 21:10:37 -0500

    create main file

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from cmu_graphics import *
+from types import SimpleNamespace
+
+msg = "Roll for initiative:"
+print(msg)
+
+print(np.random.randint(1,20))
+
+def onAppStart(app):
+    pass
+
+
+def redrawAll(app):
+    pass
+
+def main():
+    print("blehh")
+    runApp()
+
+main()
+
+
