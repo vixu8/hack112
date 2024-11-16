5cebad310b96f8d74b846e78d569fc820fe7d9fd
cjin3 <christopherqjin@gmail.com>
2024-11-16 01:26:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ print(msg)
 print(np.random.randint(1,20))
 
 def onAppStart(app):
-    app.states = "intro" #intro, build, play  
+    app.states = ["intro", 'build', 'play', 'load'] #intro, build, play, load
     app.width = 1100
     app.height = 700
     
     app.maps = [None, None, None, None]
     app.selectedMap = None
+
+    #buttons
+    app.buttonLocations = set()
+    app.buttonFunctions = {}
 
     restart(app)
 
@@ -35,19 +39,30 @@ def loadMap(app):
 #End Loading
 
 #Drawing
-
 def redrawAll(app):
-    if app.state == "testing":
+    if app.state == "build":
         drawBuildUI(app)
         drawBuildMap(app)
     elif app.state == 'intro':
         drawIntro(app)
+    elif app.state == 'load':
+        drawLoad(app)
 
 def drawIntro(app):
     drawRect(0, 0, app.width, app.height, fill='darkblue', opacity=70)
     drawLabel('STOPDOT', app.width/2, app.height/3, bold=True, size=100, font='arial', fill='white', opacity=50)
 
-    #Buttons
+    #Edit Button
+    editButtonSize = 80
+    drawRect(app.width/2-editButtonSize/2-150, app.height/2-editButtonSize/2, editButtonSize+300, editButtonSize, fill='midnightblue', opacity=80, border='black', borderWidth=5)
+    drawLabel('Edit Build', app.width/2, app.height/2, size=editButtonSize, fill='white', opacity=50)
+    newButton(app, app.width/2-editButtonSize/2-150, app.height/2-editButtonSize/2, app.width/2-editButtonSize/2-150+editButtonSize+300, app.height/2+editButtonSize/2, clickEditBuild)
+    
+    #Load Button
+    loadButtonSize = 80
+    drawRect(app.width/2-loadButtonSize/2-150, app.height/2-loadButtonSize/2 + 100, loadButtonSize+300, loadButtonSize, fill='midnightblue', opacity=80, border='black', borderWidth=5)
+    drawLabel('Load Build', app.width/2, app.height/2 + 100, size=loadButtonSize, fill='white', opacity=50)
+    newButton(app, app.width/2-loadButtonSize/2-150, app.height/2-loadButtonSize/2 + 100, app.width/2-loadButtonSize/2-150+loadButtonSize+300, app.height/2+loadButtonSize/2 + 100, clickLoadBuild)
 
 def drawBuildUI(app):
     #Menus
@@ -56,7 +71,15 @@ def drawBuildUI(app):
     drawRect(0, 0, topMenuWidth + borderWidth, 100, fill='darkblue', opacity=70, border='black', borderWidth=borderWidth) #top menu
     drawRect(topMenuWidth, 0, 200, 700, fill='darkblue', opacity=70, border='black', borderWidth=borderWidth) #side bar
     
-    #Buttons
+    #Menu Button
+    menuButtonSize = 100
+    drawLabel('Menu', 130, 50, size=menuButtonSize, fill='white', opacity=50)
+    newButton(app, 0, 0, 130+menuButtonSize+50, 50+menuButtonSize, clickMenu)
+
+def drawLoad(app):
+    drawRect(0, 0, app.width, app.height, fill='darkblue', opacity=70)
+    drawLabel('Pick a map to load', app.width/2, app.height/4, bold=True, size=100, font='arial', fill='white', opacity=50)
+
 
 
 def drawBuildMap(app):
@@ -64,6 +87,24 @@ def drawBuildMap(app):
 
 #End Drawing
 
+#Button Functions
+def newButton(app, toplx, toply, botrx, botry, func):
+    topLeft = (toplx, toply)
+    botRight = (botrx, botry)
+    location = (topLeft, botRight)
+    app.buttonLocations.add(location)
+    app.buttonFunctions[location] = func
+
+def clickMenu(app):
+    app.state = 'intro'
+
+def clickEditBuild(app):
+    app.state = 'build'
+
+def clickLoadBuild(app):
+    app.state = 'load'
+
+#End Button Functions
 
 def onStep(app):
     pass
@@ -109,6 +150,14 @@ def posToCell():
 
 def onKeyPress(app, keys):
     pass
+
+def onMousePress(app, mouseX, mouseY):
+    #check buttons
+    for location in app.buttonLocations:
+        isBetweenX = location[0][0] <= mouseX <= location[1][0]
+        isBetweenY = location[0][1] <= mouseY <= location[1][1]
+        if (isBetweenX) and (isBetweenY):
+            app.buttonFunctions[location](app)
 
 def main():
     print("blehh")
